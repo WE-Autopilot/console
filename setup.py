68fe55c51c47ff76db99dd1870617e6088c214ed
Aly Ashour <alyashour1@gmail.com>
2026-02-09 10:26:05 -0500

    Merge and fix pkg and setup issues

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
-package_name = 'ap1_control_interface'  # ROS package name
+package_name = 'ap1_console'  # ROS package name
 
 setup(
     name=package_name,                  # Python distribution name
     version='0.2.0',
     # Python package in this repo
-    packages=['control_interface'],
-    package_dir={'control_interface': 'control_interface'},
+    packages=['console'],
     # Include style.css inside the installed control_interface package
-    package_data={'control_interface': ['style.css']},
+    package_data={'console': ['style.css']},
     include_package_data=True,
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -20,13 +19,13 @@ setup(
     zip_safe=True,
     maintainer='obaidmm',
     maintainer_email='you@example.com',
-    description='AP1 control debug UI',
-    license='TODO',
+    description='AP1 console',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # ros2 run ap1_control_interface system_interface
-            'system_interface = control_interface.main:main',
+            # ros2 run ap1_console system_interface
+            'system_interface = console.main:main',
         ],
     },
 )
